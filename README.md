fbdb26280d88509ff55696fcc898dc760c14e2f1
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-07-26 10:14:06 +0530

    next init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# biosense-web
+# Biosense - Web
+IoT system for indoor plant care, monitoring health and offering personalized recommendations for growth.
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Biosense',
+  description:
+    'IoT system for indoor plant care, monitoring health and offering personalized recommendations for growth.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
